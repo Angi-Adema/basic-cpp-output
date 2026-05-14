db2c38aa2949a20b67f3a0c348ac9f631d6fc877
Angi-Adema <dr.adema@gmail.com>
2026-05-14 15:59:32 -0600

    added variables for firstName, lastName, streetAddress, city, and zipCode and initialized these with ficticious person info. Printed each of these out into the console in separatelines according to instructions.

--- basic-cpp-output/main.cpp
+++ basic-cpp-output/main.cpp
@@ -1,11 +1,23 @@
-// New C++ program
-
+// New C++ program to display basic information in the console.
 #include <iostream>
 using namespace std;
 
 int main() {
-    cout << "Hello, C++!" << endl;
-    return 0;
+	// Declare First Name, Last Name, Street Address, City, and Zip code in console
+	char firstName[] = "Mary";
+	char lastName[] = "Smith";
+	char streetAddress[] = "1234 Test Street";
+	char city[] = "Fort Bend";
+	int zipCode = 80123;
+
+	// Display stored data in the console
+	std::cout << "First Name: " << firstName << ",\n";
+	std::cout << "Last Name: " << lastName << ",\n";
+	std::cout << "Street Address: " << streetAddress << ",\n";
+	std::cout << "City: " << city << ",\n";
+	std::cout << "Zip code: " << zipCode << "\n";
+
+	return 0;
 }
 
 
